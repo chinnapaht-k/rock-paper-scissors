4b92f5da9cc927e053ff31cbdbc52976ca8bcaa1
chinnapaht <chinnapaht@gmail.com>
2023-11-27 23:32:34 +0700

    Add the getComputerChoice that return the value rock paper or scissors
    Create the getComputerChoice to return the rock paper or scissors by creating random number between 1 and 3, assign each number to each text value  and then return the text value of each number.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world!")
+function getComputerChoice(){ //Write a funtion that return rock paer or scissors
+    let numRandom = Math.floor((Math.random() * 3) + 1);//random number evenly between 1 to 3 or 0 to 2
+    let hand = numRandom == 1? "Rock" : numRandom == 2?"Paper":"Scissors" ;;
+    //if elseif and else for the number and each rockperper scissor and save to the varaible
+    console.log(numRandom)
+    return hand; //return that variable
+
+}
+console.log(getComputerChoice())
