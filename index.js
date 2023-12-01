91d43f006a0d6e97681294dbd8a8aaa1816c705b
chinnapaht <chinnapaht@gmail.com>
2023-12-01 23:39:13 +0700

    Adding the singlegame function to compare the hand of the player and computer and decide who is the winner
    Add the function that take the 2 parameter that is the hand of the player and the computer hand and decide by conditonal that which hand will win, tie or lose

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ function getComputerChoice(){ //Write a funtion that return rock paer or scissor
     let numRandom = Math.floor((Math.random() * 3) + 1);//random number evenly between 1 to 3 or 0 to 2
     let hand = numRandom == 1? "Rock" : numRandom == 2?"Paper":"Scissors" ;;
     //if elseif and else for the number and each rockperper scissor and save to the varaible
-    console.log(numRandom)
     return hand; //return that variable
 
 }
-console.log(getComputerChoice())
+
+
+function singleGame(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    console.log(computerSelection)
+    //Compare the hand of the computer and player and decide which player win
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock")||
+    (playerSelection == "scissor" && computerSelection == "paper")) {
+        return `You win! ${playerSelection} beat ${computerSelection}`
+    }
+    else if (playerSelection == computerSelection) {
+        return `Tie! ${playerSelection} equal ${computerSelection}`
+    }
+    else {
+        return `You Lose! ${playerSelection} lose to ${computerSelection}`
+    }
+}
+
+
+
+
