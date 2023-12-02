f499adbca449baafad14fab731338b1a1ca3d428
chinnapaht <chinnapaht@gmail.com>
2023-12-02 14:45:09 +0700

    Add the game function that create the best of five game of rock paaper scissors
    Create the game function that take the playerhand from the input and the computer from the getComputerChoice and compared which will win from the single game function. then compared the value in win and lose varaible and run all of this 5 times in a loop and then compared who will win outside that loop

--- index.js
+++ index.js
@@ -10,21 +10,51 @@ function getComputerChoice(){ //Write a funtion that return rock paer or scissor
 function singleGame(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
-    console.log(computerSelection)
     //Compare the hand of the computer and player and decide which player win
     if ((playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "paper" && computerSelection == "rock")||
     (playerSelection == "scissor" && computerSelection == "paper")) {
-        return `You win! ${playerSelection} beat ${computerSelection}`
+        console.log(`You win! ${playerSelection} beat ${computerSelection}`);
+        return "win";
     }
     else if (playerSelection == computerSelection) {
-        return `Tie! ${playerSelection} equal ${computerSelection}`
+        console.log(`Tie! ${playerSelection} equal ${computerSelection}`);
+        return "tie"
     }
     else {
-        return `You Lose! ${playerSelection} lose to ${computerSelection}`
+        console.log(`You Lose! ${playerSelection} lose to ${computerSelection}`)
+        return "lose"
     }
 }
 
 
+function game(){
+    let userHand, computerHand ;
+    let win = 0, lose = 0;
+    for (let  i = 0; i < 5; i++){//loop the game 5 time
+        userHand = prompt("Rock, Paper or Scissors?: ") //take input from the player
+        computerHand = getComputerChoice();
+        console.log(`Computer choose : ${computerHand}`)
+        if (singleGame(userHand, computerHand) === "win"){
+            win += 1
+        }
+        else if (singleGame(userHand, computerHand) === "lose")
+            lose += 1
+    }
+    if (win > lose){
+        console.log("You win! Congratulation!")
+    }
+    else if (lose > win){
+        console.log("Unforturnately you have lose. Please try again later :)")
+    }
+    else {
+        console.log("It is a tied. Good job!")
+    }
+        //keeptrack of the winning by the format of best of five ---> if win>=3 win if win=lose lose else lose
+        
+}
 
+
+
+game()
 
